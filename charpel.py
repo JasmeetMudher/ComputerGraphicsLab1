b58a5fc9bd34f46e96018cce3ecad297127c3751
cliffkoome <koomekoome902@gmail.com>
2024-10-18 13:02:06 +0300

    cross and pillar and top window

--- charpel.py
+++ charpel.py
@@ -5,6 +5,43 @@ ctx = cairo.Context(surface)
 ctx.set_source_rgb(1, 1, 1)
 ctx.paint()
 
+#Cross
+ctx.set_source_rgb(0,0,0)
+ctx.move_to(400,50)
+ctx.line_to(395,50)
+ctx.line_to(395,65)
+ctx.line_to(375,65)
+ctx.line_to(375,75)
+ctx.line_to(395,75)
+ctx.line_to(395,105)
+ctx.line_to(405,105)
+ctx.line_to(405,75)
+ctx.line_to(425,75)
+ctx.line_to(425,65)
+ctx.line_to(405,65)
+ctx.line_to(405,50)
+ctx.close_path()
+ctx.fill()
+
+
+#CrossPillar
+ctx.move_to(395,105)
+ctx.line_to(385,120)
+ctx.line_to(375,200)
+ctx.line_to(425,200)
+ctx.line_to(415,120)
+ctx.line_to(405,105)
+ctx.close_path()
+ctx.fill()
+
+#Top window
+ctx.rectangle(330,200,140,10)
+ctx.set_source_rgb(1,1,1)
+ctx.stroke_preserve()
+ctx.set_source_rgb(0,0,0)
+ctx.fill()
+
+
 # Draw the house base (black rectangle)
 ctx.set_source_rgb(0, 0, 0)  # Set color to black
 ctx.rectangle(100, 800, 800, 150)  # (x, y, width, height)
